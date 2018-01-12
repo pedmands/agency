5e9c16fb189b8d744a72c8bce05e72d531e15048
Preston Edmands <preston@Prestons-MacBook-Pro.local>
2018-01-11 21:38:13 -0500

    Installed _s

---
+++ inc/customizer.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * Agency Underscores Theme Customizer
+ *
+ * @package Agency_Underscores
+ */
+
+/**
+ * Add postMessage support for site title and description for the Theme Customizer.
+ *
+ * @param WP_Customize_Manager $wp_customize Theme Customizer object.
+ */
+function agency_underscores_customize_register( $wp_customize ) {
+	$wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
+	$wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
+	$wp_customize->get_setting( 'header_textcolor' )->transport = 'postMessage';
+
+	if ( isset( $wp_customize->selective_refresh ) ) {
+		$wp_customize->selective_refresh->add_partial( 'blogname', array(
+			'selector'        => '.site-title a',
+			'render_callback' => 'agency_underscores_customize_partial_blogname',
+		) );
+		$wp_customize->selective_refresh->add_partial( 'blogdescription', array(
+			'selector'        => '.site-description',
+			'render_callback' => 'agency_underscores_customize_partial_blogdescription',
+		) );
+	}
+}
+add_action( 'customize_register', 'agency_underscores_customize_register' );
+
+/**
+ * Render the site title for the selective refresh partial.
+ *
+ * @return void
+ */
+function agency_underscores_customize_partial_blogname() {
+	bloginfo( 'name' );
+}
+
+/**
+ * Render the site tagline for the selective refresh partial.
+ *
+ * @return void
+ */
+function agency_underscores_customize_partial_blogdescription() {
+	bloginfo( 'description' );
+}
+
+/**
+ * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
+ */
+function agency_underscores_customize_preview_js() {
+	wp_enqueue_script( 'agency_underscores-customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), '20151215', true );
+}
+add_action( 'customize_preview_init', 'agency_underscores_customize_preview_js' );
